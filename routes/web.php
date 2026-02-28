0e032921ec914d52fbfb167d77677ac0bfbc5bbc
Suzzanne20 <susanacevedo11@hotmail.com>
2026-02-28 16:34:35 -0600

    otras otros cambios

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 
 Route::view('/', 'home')->name('home');
 Route::view('/soluciones', 'soluciones')->name('soluciones');
-=======
-Route::view('/', 'home')->name('home');
-Route::view('/soluciones', 'soluciones')->name('soluciones');
->>>>>>> 11248ecdcb6735b14542e98066b28e13eb860149
